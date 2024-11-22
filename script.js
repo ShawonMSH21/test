707e8f20bc46eacee735e6ad8c5ba8cf03317a4c
Shawon Hossain <shawonhossainkabeer.msh@gmail.com>
2024-11-22 22:53:53 +0600

    hello

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.cta-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Redirecting you to the Cash App sign-up page. Complete the steps to claim your bonus!");
+    });
+});
